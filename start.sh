31c2e1727fe7ca3e258139b4e3b305eb28196743
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-17 13:32:13 +0300

    DGokf

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-export SECRET_KEY='01570936bd6b231dd1e1037bb224cd'
+export SECRET_KEY='01570936bd6b231dd1e14cd'
 export MAIL_USERNAME='lewismutuma1000@gmail.com'
 export MAIL_PASSWORD='533253325332'
 
