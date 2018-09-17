965cc43eddf59a5c9aeafa194befc959fadee6f8
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-17 13:57:56 +0300

    deploy

--- app/main/views.py
+++ app/main/views.py
@@ -44,6 +44,7 @@ def update_profile(uname):
         return redirect(url_for('.profile',uname=user.username))
 
     return render_template('profile/update.html',form =form)
+    
 @main.route('/user/<uname>/update/pic',methods= ['POST'])
 @login_required
 def update_pic(uname):
