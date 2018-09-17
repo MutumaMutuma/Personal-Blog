375d7200cd46c5538b51d1817f1a57c4bf1ca94e
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-17 18:24:53 +0300

    ece

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ from flask_script import Manager,Server
 from app.models import User, Comment, Post
 from flask_migrate import Migrate , MigrateCommand
 
-app = create_app('production')
-# app = create_app('development')
+# app = create_app('production')
+app = create_app('development')
 
 
 manager = Manager(app)
