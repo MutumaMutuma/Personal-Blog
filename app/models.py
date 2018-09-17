cd17d048a253af0405a4dcb79e1d8ea2110e1f85
MutumaMutuma <lewismutuma1000@gmail.com>
2018-09-17 13:48:33 +0300

    migrated

--- app/models.py
+++ app/models.py
@@ -17,7 +17,6 @@ class User(UserMixin, db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     pass_secure = db.Column(db.String(255))
-    subscribers = db.Column(db.Integer)
 
     posts = db.relationship('Post',backref='user',lazy='dynamic')
     comments = db.relationship('Comment', backref = 'comment', lazy= "dynamic")
